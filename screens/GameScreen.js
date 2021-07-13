e92792492b7b35a76d739ff9ebdbe4a520e2f9f9
gk009-dev <gk112654@gmail.com>
2021-07-13 11:39:13 +0530

    alert,gamestart for app

---
+++ screens/GameScreen.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {StyleSheet,Text,View} from 'react-native';
+
+const GameScreen = props =>{
+
+
+
+}
+
+const styles = StyleSheet.create({
+
+});
+
+export default GameScreen;
